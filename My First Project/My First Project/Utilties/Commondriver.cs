e64af0a85a28de7355de38426a580c4a8b7350cc
Anitha V <anithainfo9@gmail.com>
2022-08-03 01:22:22 +1200

    Commondriver added in the utilities

---
+++ My First Project/My First Project/Utilties/Commondriver.cs
@@ -0,0 +1,45 @@
+﻿using My_First_Project.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Project.Utilties
+{
+    public class CommonDriver
+    {
+        public static IWebDriver driver;
+        LoginPage loginpageobj = new LoginPage();
+
+    
+
+    [OneTimeSetUp]
+    public void LoginActions()
+    {
+        //Open Chrome Browser
+        IWebDriver driver = new ChromeDriver();
+
+        //Login page object initialization and definition
+        LoginPage loginpageobj = new LoginPage();
+        loginpageobj.LoginSteps(driver);
+
+    }
+
+    [OneTimeTearDown]
+    public void CloseTestRun()
+    {
+        driver.Close();
+        //driver.Quit();
+
+       // instead of driverQuit using driverclose
+
+    }
+    }
+}
+
+
+//static returns the value
